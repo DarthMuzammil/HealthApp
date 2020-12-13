fd5191ef56b12603af05c45e898a06f5fd6a6225
DarthMuzammil <muzammil.inamdar98@gmail.com>
2020-12-13 18:24:17 +0530

    last

--- HealthTech/src/screens/Chat.js
+++ HealthTech/src/screens/Chat.js
@@ -1,50 +1,84 @@
 import React, { Component, useState } from 'react';
 import {
-  SafeAreaView,
-  View,
-  Text
-  ,StatusBar,
-  LogBox
+    SafeAreaView,
+    View,
+    Text
+    , StatusBar,
+    LogBox
 } from 'react-native';
 
 import AwesomeChat from 'react-native-awesome-chat';
 
 export class ChatInterface extends React.Component {
-  constructor(props){
-      super(props);
-      this.state = {
-          messages : [
-              {body : "Hello", id : 1,
-              timestamp : 1581418856, type : "sent", image_uri : ""},
-              {body : "Hi", id : 2, 
-              timestamp : 1581418856, type : "received", image_uri : ""}
-          ],
-      }
-  }
-  
-  sendMessage = async (message) => {
-    console.log(message)
-    return true;
-  }
 
-  render(){
-      return (
-      <>
-          <StatusBar barStyle="dark-content" />
-          <SafeAreaView>
-          <View style={{height : '100%'}}>
-              <AwesomeChat 
-                  onSendMessage={this.sendMessage} 
-                  messages={this.state.messages}
-              />
-          </View>
-          </SafeAreaView>
-      </>
-      );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [
+                {
+                    body: "Hello", id: 1,
+                    timestamp: 1581418856, type: "sent", image_uri: ""
+                },
+                {
+                    body: "Hi", id: 2,
+                    timestamp: 1581418856, type: "received", image_uri: ""
+                },
+            ],
+        }
+    }
+    try = () => {
+        fetch('https://chatbotmed.herokuapp.com/', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson)
+
+            });
+    }
+    sendMessage = async (message) => {
+        fetch('https://chatbotmed.herokuapp.com/', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson["response"])
+                /*this.state.messages.push(  {body: responseJson["response"], id: 9,
+                timestamp: 1581418856, type: "sent", image_uri: ""})*/
+
+            });
+            console.log(this.state.messages)
+        return true;
+    }
+
+    render() {
+        return (
+            <>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+                    <View style={{ height: '100%' }}>
+                        <AwesomeChat
+                            onSendMessage={this.sendMessage}
+                            messages={[
+                                {
+                                    body: "Hello", id: 1,
+                                    timestamp: 1581418856, type: "sent", image_uri: ""
+                                },]}
+                        />
+                    </View>
+                </SafeAreaView>
+            </>
+        );
+    }
 };
 
-function Chat(navigation){
-    return(<ChatInterface />)
+function Chat(navigation) {
+    return (<ChatInterface />)
 }
 export default Chat;
--- HealthTech/src/screens/Homepage.js
+++ HealthTech/src/screens/Homepage.js
@@ -18,27 +18,13 @@ const styles = StyleSheet.create({
     }
 });
 function Homepage({ navigation }) {
-    function cal() {
-        fetch('http://127.0.0.1:5000', {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }).then((response) => response.json())
-        .then((responseJson) => {
-  console.log(responseJson)
-  
-        });
-    }
-    cal()
     return (
         <>
             <StatusBar barStyle="dark-content" />
             <SafeAreaView>
                 <View style={{ height: '100%' }}>
                     <Text>Click here to go to bot</Text>
-                    <TouchableOpacity style={styles.randombutton} onPress={() => { cal() }}></TouchableOpacity>
+                    <TouchableOpacity style={styles.randombutton} onPress={() => { navigation.navigate('Chat') }}></TouchableOpacity>
                 </View>
             </SafeAreaView>
         </>
